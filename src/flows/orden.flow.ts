cd22ebd4e493738f2346c5faf4a485c4906584aa
kvndan <dannyelctro@gmail.com>
2024-06-30 00:16:21 -0500

    v0.1-api-004

--- src/flows/orden.flow.ts
+++ src/flows/orden.flow.ts
@@ -177,7 +177,7 @@ const flowOrden = addKeyword(EVENTS.ACTION)
 
         const fullPath = response.data.pdfPath;
         const pdfPath = fullPath.split("\\").pop().split("/").pop();
-        const pdfUrl = `https://api-catalogo-pdf.onrender.com/pdfs/${pdfPath}`;
+        const pdfUrl = `https://api-catalogo-pdf.onrender.com/api/pdfs/${pdfPath}`;
         console.log("Pedido creado:", JSON.stringify(response.data, null, 2));
         console.log("URL del PDF:", pdfUrl);
 
